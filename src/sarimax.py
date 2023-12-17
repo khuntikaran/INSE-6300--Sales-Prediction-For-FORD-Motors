3faab326e996049701836b2b9288b5341f012443
khuntikaran <69393942+khuntikaran@users.noreply.github.com>
2023-12-18 05:27:52 +0530

    sarimax.py

--- src/sarimax.py
+++ src/sarimax.py
@@ -4,31 +4,24 @@ from statsmodels.tsa.statespace.sarimax import SARIMAX
 import matplotlib.pyplot as plt
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 
-# Load your dataset
+
 data = pd.read_csv('FordM.csv')
-
-# Assuming 'Sales' is your target variable and 'data' is your DataFrame
-# Replace 'exog_variable' with actual external variables if you have any
-# exog = data['exog_variable']
-
-# Define the SARIMAX model
-# Modify these parameters according to your dataset's seasonality
 model = SARIMAX(data['Sales'],
-                order=(1, 1, 1),              # These are the non-seasonal ARIMA parameters: (p, d, q)
-                seasonal_order=(1, 1, 1, 12), # These are the seasonal ARIMA parameters: (P, D, Q, S)
-                # exog=exog                   # Uncomment if you have external variables
+                order=(1, 1, 1),             
+                seasonal_order=(1, 1, 1, 12),
+                # exog=exog                  
                )
 
-# Fit the model
+
 model_fit = model.fit()
 
-# Summary of the model
+
 print(model_fit.summary())
 
-# Forecasting
+
 data['Forecast'] = model_fit.predict(start=1, end=len(data), typ='levels')
 
-# Plotting the results
+
 plt.figure(figsize=(12, 6))
 plt.plot(data['Sales'], label='Actual Sales')
 plt.plot(data['Forecast'], color='red', label='Forecasted Sales')
